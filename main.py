cf35d259c6c28912709c96107e43b6543cf7ef8e
Kris Renaldi <krenaldi@comcast.net>
2024-09-04 14:55:34 -0400

    Setup Player class in groups to hold all drawable and updatable objects

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = updatable, drawable
     player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, PLAYER_RADIUS)
     dt = 0
 
@@ -20,8 +23,12 @@ def main():
                 running = False
 
         screen.fill((0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
+        for obj in updatable:
+            obj.update(dt)
+        # player.update(dt)
+        # player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
