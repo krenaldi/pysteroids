616adfb8409ae5fa7646287f82c52dac2db2d7a3
Kris Renaldi <krenaldi@comcast.net>
2024-09-03 20:57:40 -0400

    Added pygame.time.Clock object to control fps on window

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ def main():
     # print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     running = True
-
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -17,6 +18,7 @@ def main():
 
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
     pygame.quit()
 
